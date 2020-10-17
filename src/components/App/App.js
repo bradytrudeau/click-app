83a809ddc8796229bcb15bf761c37bff584cdd8e
Brady Trudeau <bradytrudeau@gmail.com>
2020-10-17 14:51:43 -0500

    Messing with tabs and broke app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,8 @@ import {
   Route,
   Redirect,
   Switch,
+  Link,
+  withRouter
 } from 'react-router-dom';
 
 import { connect } from 'react-redux';
@@ -22,111 +24,122 @@ import Metronome from '../Metronome/Metronome';
 import SongForm from '../SongForm/SongForm';
 import SongLibrary from '../SongLibrary/SongLibrary';
 import EditSongItem from '../EditSongItem/EditSongItem';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Paper from '@material-ui/core/Paper';
+import LogOutButton from '../LogOutButton/LogOutButton';
+
 
 import './App.css';
+import mapStoreToProps from '../../redux/mapStoreToProps';
+import { text } from 'express';
 
 class App extends Component {
+
   componentDidMount() {
     this.props.dispatch({ type: 'FETCH_USER' });
   }
+  
+
 
   render() {
     return (
-      <Router>
-        <div>
-          <Nav />
+      <div>
+        <div className="nav-right">
+          <Router>
+          {!this.props.store.user.id && (
+            <>
+              <Link className="nav-link" to="/login">Login / Register</Link>
+            </>
+          )}
+          {this.props.store.user.id && (
+            <>
+              <LogOutButton className="nav-link" />
+            </>
+          )}
+          </Router>
+        </div>
+        {/* {!this.props.store.user.id && (
+          <h1>WELCOME TO THE CLICK APP!</h1>
+        )} */}
+        <Router>
+          {this.props.store.user.id && (
+            <Route 
+              render={(history) => (
+              <Paper>
+                <Tabs
+                  value={history.location.pathname}
+                  indicatorColor="primary"
+                  textColor="primary"
+                  centered
+                >
+                  <Tab 
+                    label="Home"
+                    value="/home"
+                    component={Link}
+                    to="/home"
+                  />
+                  <Tab 
+                    label="Metronome"
+                    value="/metronome"
+                    component={Link}
+                    to="/metronome"
+                  />
+                  <Tab 
+                    label="Add a Song"
+                    value="/songform"
+                    component={Link}
+                    to="/songform"
+                  />
+                  <Tab 
+                    label="Song Library"
+                    value="/songlibrary"
+                    component={Link}
+                    to="/songlibrary"
+                  />
+                </Tabs>
+              </Paper>
+              )}
+              />
+            )}
           <Switch>
-            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
             <Redirect exact from="/" to="/home" />
-
-            {/* Visiting localhost:3000/about will show the about page. */}
-            <Route
-              // shows AboutPage at all times (logged in or not)
-              exact
-              path="/about"
-              component={AboutPage}
-            />
-
-            <ProtectedRoute
-              // shows Metronome at all times (logged in or not)
-              exact
-              path="/metronome"
+            <ProtectedRoute 
+              path="/metronome" 
               component={Metronome}
             />
-
-            <ProtectedRoute
-              // shows Metronome at all times (logged in or not)
-              exact
-              path="/edit"
-              component={EditSongItem}
-            />
-
-            <ProtectedRoute
-              // shows Metronome at all times (logged in or not)
-              exact
-              path="/songlibrary"
-              component={SongLibrary}
-            />
-
-            <ProtectedRoute
-              // shows Metronome at all times (logged in or not)
-              exact
-              path="/songform"
+            <ProtectedRoute 
+              path="/songform" 
               component={SongForm}
             />
-
-            {/* <ProtectedRoute
-              // logged in shows UserPage else shows LoginPage
-              exact
-              path="/user"
-              component={UserPage}
-            /> */}
-
-            <ProtectedRoute
-              // logged in shows InfoPage else shows LoginPage
-              exact
-              path="/info"
-              component={InfoPage}
+            <ProtectedRoute 
+              path="/songlibrary" 
+              component={SongLibrary}
             />
-
-            {/* When a value is supplied for the authRedirect prop the user will
-            be redirected to the path supplied when logged in, otherwise they will
-            be taken to the component and path supplied. */}
             <ProtectedRoute
-              // with authRedirect:
-              // - if logged in, redirects to "/user"
-              // - else shows LoginPage at /login
               exact
               path="/login"
               component={LoginPage}
               authRedirect="/home"
-            />
+            />  
             <ProtectedRoute
-              // with authRedirect:
-              // - if logged in, redirects to "/user"
-              // - else shows RegisterPage at "/registration"
               exact
               path="/registration"
               component={RegisterPage}
               authRedirect="/home"
             />
             <ProtectedRoute
-              // with authRedirect:
-              // - if logged in, redirects to "/user"
-              // - else shows LandingPage at "/home"
               exact
               path="/home"
               component={LandingPage}
             />
-
-            {/* If none of the other routes matched, we will show a 404. */}
             <Route render={() => <h1>404</h1>} />
           </Switch>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+        <Footer/>
+      </div>
     );
   }
 }
 
-export default connect()(App);
+export default connect(mapStoreToProps)(App);
